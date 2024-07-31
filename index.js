8893f7dd6843f6eae2187ddb090e6677132e421f
Steven Choy <stevenchoy0915@gmail.com>
2024-07-31 19:17:20 +0800

    added add function

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("Hello World");
 console.log("Hello Partner A");
 console.log("Kumusta Partner B");
+
+console.log("anything");
+function add(a, b) {
+  return a + b;
+}
+console.log(add(1, 2));
