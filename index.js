cf16effe774cb4c299abc836dae5635d1cb0bc36
Steven Choy <stevenchoy0915@gmail.com>
2024-07-31 19:19:35 +0800

    added subtract function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log("Hello World");
 console.log("Hello Partner A");
 console.log("Kumusta Partner B");
+
+console.log("something");
+function subtract(a, b) {
+  return a - b;
+}
+
+console.log(subtract(5, 4));
